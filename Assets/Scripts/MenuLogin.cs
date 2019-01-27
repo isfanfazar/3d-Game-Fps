390cbde1200ee953e568545a5d32ceeef2742503
isfanfazar <isfanfazar.ifs@gmail.com>
2019-01-27 08:04:54 +0700

    My Project Game

---
+++ Assets/Scripts/MenuLogin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLogin : MonoBehaviour {
+
+	public GameObject menuPanel;
+	public GameObject howToPanel;
+	public GameObject creditPanel;
+	
+	// Use this for initialization
+	void Start () {
+		menuPanel.SetActive(true);
+		howToPanel.SetActive(false);
+		creditPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void StartGameClicked(){
+		Application.LoadLevel(1);
+	}
+	
+	public void HowToClicked(){
+		howToPanel.SetActive(true);
+		creditPanel.SetActive(false);
+		menuPanel.SetActive(false);
+	}
+	
+	public void CreditClicked(){
+		creditPanel.SetActive(true);
+		menuPanel.SetActive(false);
+		howToPanel.SetActive(false);
+	}
+	
+	public void QuitGameClick(){
+		Application.Quit();
+	}
+	
+	public void BackToMenuClicked(){
+		menuPanel.SetActive(true);
+		howToPanel.SetActive(false);
+		creditPanel.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	public string EnterS;
+	public string QuitS;
+	public bool isEscapeForQuit = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Return))
+		{
+			Debug.Log("Name Scene: " + EnterS);
+			SceneManager.LoadScene(EnterS);
+		}
+		
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			if (isEscapeForQuit)
+			{
+				Application.Quit();
+			}
+			else 
+			{
+				Debug.Log("Name Scene: " + QuitS);
+				SceneManager.LoadScene(QuitS);
+			}
+		}
+	}
+}
